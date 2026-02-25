9cb084d704624bb558decc378cd9834b104f20b2
Luis Espinal <luisjavierespinal23@gmail.com>
2026-02-25 03:10:16 -0400

    fix: configurar base path para quitar pantalla en blanco

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dosesync2/', 
+  base: '/dosesync2/', // <--- AGREGA ESTA LÍNEA EXACTA
 })
-
